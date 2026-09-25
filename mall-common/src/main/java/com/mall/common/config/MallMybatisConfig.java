45469fdbdfd9bb229912f92a6faa6372bc8b0e9a
lizhenghang <1135365441@qq.com>
2026-09-25 21:18:00 +0800

    食谱

--- mall-common/src/main/java/com/mall/common/config/MallMybatisConfig.java
+++ mall-common/src/main/java/com/mall/common/config/MallMybatisConfig.java
@@ -3,9 +3,6 @@ package com.mall.common.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * 扫描业务 Mapper（jeecg 默认只扫 org.jeecg.**.mapper*）
- */
 @Configuration
 @MapperScan(value = {"com.mall.**.mapper*"})
 public class MallMybatisConfig {
---
+++ mall-common/src/main/java/com/mall/common/constant/ToolStatusConstant.java
@@ -0,0 +1,16 @@
+package com.mall.common.constant;
+
+public class ToolStatusConstant {
+    public static final String DONE = "done";
+    public static final String RUNNING = "running";
+
+    public static final String TOOL = "tool";
+    public static final String TOOL_CALL_DELTA = "tool_call_delta";
+    public static final String TOOL_CALL_END = "tool_call_end";
+    public static final String TOOL_RESULT_START = "tool_result_start";
+    public static final String TOOL_CALL_START = "tool_call_start";
+    public static final String THINKING_DELTA = "thinking_delta";
+    public static final String ANSWER_START = "answer_start";
+    public static final String ANSWER_DELTA = "answer_delta";
+    public static final String TOOL_RESULT_END = "tool_result_end";
+}
